d39ce5fa8cb95ae2c4d480ce196a97e77854b89c
koziro2000 <koziro2000@gmail.com>
2017-06-18 20:33:33 -0400

    punching works but not natural

--- state0.js
+++ state0.js
@@ -1,9 +1,10 @@
 var demo = {}, centerX = 1500/2, centerY = 1000/2, adam, speed = 6;
-var isSwingSowrd = false, swingTime = 0;
+var isSwingSowrd = false, swingTime = 0, swingDuration = 10;
+var scaleSetVal = 1;
 demo.state0 = function() {};
 demo.state0.prototype = {
     preload: function(){
-        game.load.spritesheet('adam', 'assets/spritesheets/adamSheet.png', 240, 370);
+        game.load.spritesheet('adam', 'assets/spritesheets/Daddy.png', 320, 320);
         game.load.image('tree', 'assets/backgrounds/treeBG.png');
     },
     create:  function(){        
@@ -16,53 +17,56 @@ demo.state0.prototype = {
         var treeBG = game.add.sprite(0, 0, 'tree');
         adam = game.add.sprite(centerX, centerY, 'adam');
         adam.anchor.setTo(0.5, 0.5);
-        adam.scale.setTo(0.7, 0.7);
+        adam.scale.setTo(scaleSetVal, scaleSetVal);
         
         game.physics.enable(adam);
         adam.body.collideWorldBounds = true;
-        adam.animations.add('walk', [0, 1, 2, 3, 4]);
+        adam.animations.add('walk', [0, 1, 2, 3]);
+        adam.animations.add('punch', [4, 5, 6]);
         game.camera.follow(adam);
         game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            adam.scale.setTo(0.7, 0.7);
+            adam.scale.setTo(scaleSetVal, scaleSetVal);
             adam.x += speed;
             adam.animations.play('walk', 14, true);
         } 
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            adam.scale.setTo(-0.7, 0.7);
+            adam.scale.setTo(-scaleSetVal, scaleSetVal);
             adam.x -= speed;
             adam.animations.play('walk', 14, true);
         }
         else {
             adam.animations.stop('walk');
-            adam.frame = 0;
+            if(!isSwingSowrd) adam.frame = 0;
         }
 
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
             adam.y -= speed;
             if (adam.y < 395) {
                 adam.y = 395;
-            }
+            }            
         } 
         else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-            adam.y += speed;
+            adam.y += speed;            
         }
         
         if((game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))) {
             if(!isSwingSowrd && swingTime == 0) {
-                console.log('Swing Sword');                
+                adam.animations.play('punch');
                 isSwingSowrd = true;
             } else {
                 console.log('Already Swinging');
             }
         }
         
-        if (isSwingSowrd && swingTime >= 60) {
+        if (isSwingSowrd && swingTime >= swingDuration) {
             isSwingSowrd = false;
             swingTime = 0;
-        } else if (isSwingSowrd && swingTime < 60) {
+            adam.animations.stop('punch');
+            adam.frame = 0;
+        } else if (isSwingSowrd && swingTime < swingDuration) {
             swingTime += 1;
         }
     }
