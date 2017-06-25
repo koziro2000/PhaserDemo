e1885122884a7eff07c790e6e03dd97b7930b9f9
koziro2000 <koziro2000@gmail.com>
2017-06-24 23:47:11 -0400

    Playing with fliping lotating and scaling

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
+var GameState = {
+    preload: function() {
+        this.load.image('background', 'assets/images/background.png');
+        this.load.image('chicken', 'assets/images/chicken.png');
+        this.load.image('horse', 'assets/images/horse.png');
+        this.load.image('pig', 'assets/images/pig.png');
+        this.load.image('sheep', 'assets/images/sheep.png');
+        
+    },
+    create: function() {
+        this.background = this.game.add.sprite(0, 0, 'background');
+        
+        this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+        this.chicken.anchor.setTo(0.5);
+        this.chicken.scale.setTo(2, 1);
+        
+        this.horse = this.game.add.sprite(120, 10, 'horse');
+        this.horse.scale.setTo(0.5);
+        
+        this.pig = this.game.add.sprite(500, 300, 'pig');
+        this.pig.anchor.setTo(0.5);
+        this.pig.scale.setTo(-1, 1);
+    },
+    update: function() {
+        
+    }
+};
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
